012ac01eb0c2c6f53d1bd9b5705f0238b32c00a4
stella <stella2837@naver.com>
2023-04-25 19:30:02 +0900

    add Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link, Switch} from 'react-router-dom';  // eslint-disable-line no-unused-vars 
+import MainHome from './mainPage';
+import SignIn from './SignIn';
+
+
 
 function App() {
-  const [jsonData, setHello] = useState('')
-
-  useEffect(() => {
-      axios.get('/api/test')
-      .then(response => setHello(response.data))
-      .catch(error => console.log(error))
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          MITRE ATT&CK Json file
-        </p>
-        <div>
-            {JSON.stringify(jsonData)}
-        </div>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<MainHome />}></Route>
+        <Route path = "SignIn" element = {<SignIn />}></Route>
+      </Routes>
+
+      
+
+    </Router>
   );
 }
 
---
+++ frontend/src/mainPage.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Routes, Route, Link, Switch} from 'react-router-dom';  // eslint-disable-line no-unused-vars 
+import axios from 'axios';
+import './App.css';
+
+function MainPage(){
+    const [jsonData, setHello] = useState('')
+
+  useEffect(() => {
+      axios.get('/api/test')
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
+    return(
+        <div>
+            <header className="App-header">
+                <h3>This is main Page</h3>
+
+                <Link to = './SignIn'>SignIn</Link>
+
+                <p>
+                    MITRE ATT&CK Json file
+                </p>
+
+                <div>
+                    {JSON.stringify(jsonData)}
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default MainPage;
