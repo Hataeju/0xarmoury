c4ebefc34bda3c13ea00fc6d52293adb50d4160d
wjdgus06 <63927229+wjdgus06@users.noreply.github.com>
2023-04-27 14:15:18 +0900

    validation, post, Register error message

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,10 +12,80 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import FormHelperTexts from '@mui/material/FormHelperText';
 
 const theme = createTheme();
 
+
 export default function SignUp() {
+
+  const [emailError, setEmailError] = useState('');
+  const [passwordState, setPasswordState] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [registerError, setRegisterError] = useState('');
+  const Navigate = useNavigate();
+
+  const onhandlePost = async (data) => {
+    const { email, firstName, lastNmae, nickname, password } = data;
+    const postData = { email, firstName, lastNmae, nickname, password };
+
+    // post
+    await axios
+      .post('/member/join', postData) //API 입력
+      .then(function (response) {
+        console.log(response, '성공');
+        Navigate.push('/SignIn');
+      })
+      .catch(function (err) {
+        console.log(err);
+        setRegisterError('회원가입에 실패하였습니다. 다시한번 확인해 주세요.');
+      });
+      // TODO: 입력된 정보를 서버에 전송하는 로직 작성
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault(); // 기본 이벤트 동작 취소
+
+    const data = new FormData(event.currentTarget);
+    const joinData = {
+      email: data.get('email'),
+      name: data.get('name'),
+      password: data.get('password'),
+      rePassword: data.get('rePassword'),
+      firstname: data.get('firstName'),
+      lastName: data.get('lastName'),
+      nickname: data.get('nickname')
+    };
+
+    //이건 왜 하는데?
+    const  { email, firstName, lastNmae, nickname, password, rePassword } = joinData;
+
+    // 유효성 검사
+
+    //이메일 유효성 체크
+    const emailRegex = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (!emailRegex.test(email)) setEmailError('올바른 이메일 형식이 아닙니다.');
+    else setEmailError('');
+
+    // 비밀번호 유효성 체크
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    if (!passwordRegex.test(password))
+      setPasswordState('숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!');
+    else setPasswordState('');
+
+    // 비밀번호 같은지 체크
+    if (password !== rePassword) setPasswordError('비밀번호가 일치하지 않습니다.');
+    else setPasswordError('');
+
+    if (
+      emailRegex.test(email) &&
+      passwordRegex.test(password) &&
+      password === rePassword
+    ) {
+      onhandlePost(joinData); // 회원가입 정보 post
+    }
+  }
   return (
     <ThemeProvider theme={theme}>
     {/*Home button*/}
@@ -44,7 +114,7 @@ export default function SignUp() {
           <Typography component="h1" fontSize={'30px'}>
             <strong>Sign Up</strong>
           </Typography>
-          <Box component="form" noValidate sx={{ mt: 3 }}>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -72,7 +142,7 @@ export default function SignUp() {
                   required
                   fullWidth
                   id="nickname"
-                  label="Nickname"
+                  label="Nickame"
                   name="nickname"
                   autoComplete="nickname"
                 />
@@ -85,8 +155,10 @@ export default function SignUp() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  error={emailError !== '' || false}
                 />
               </Grid>
+              <FormHelperTexts>{emailError}</FormHelperTexts>
               <Grid item xs={12}>
                 <TextField
                   required
@@ -96,31 +168,34 @@ export default function SignUp() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  error={passwordState !== '' || false}
                 />
               </Grid>
+              <FormHelperTexts>{passwordState}</FormHelperTexts>
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  name="confirmPassword"
+                  name="rePassword"
                   label="Confirm Password"
                   type="Password"
-                  id="confirmPassword"
-                  autoComplete="new-confirmPassword"
+                  id="rePassword"
+                  error={passwordError !== '' || false}
                 />
               </Grid>
-              
+              <FormHelperTexts>{passwordError}</FormHelperTexts>
             </Grid>
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2, bgcolor:"#363636" }}
+              sx={{ mt: 3, mb: 1, bgcolor:"#363636" }}
             >
               Sign Up
             </Button>
+            <FormHelperTexts sx={{color: '#DB0000'}}>{registerError}</FormHelperTexts>
             <Grid container justifyContent="flex-end">
-              <Grid item>
+              <Grid item sx={{mt:1}}>
                 <Link href="./SignIn" variant="body2" color='#0000A5'>
                   {"Already have an account? Sign in"}
                 </Link>
@@ -128,7 +203,6 @@ export default function SignUp() {
             </Grid>
           </Box>
         </Box>
-
       </Container>
     </ThemeProvider>
   );
